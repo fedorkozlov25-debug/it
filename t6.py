53915048679d30465b0cdec8d20eca1f46f5a04b
Fedor <fedorkozlov25@gmail.com>
2026-01-11 15:54:30 +0300

    message

--- t6.py
+++ t6.py
@@ -1,7 +1,7 @@
 import pandas as pd
 data=pd.read_csv('titanic.csv')
 data['FamilySize'] = data['SibSp'] + data['Parch'] + 1
-pas=data[data['PassengerId']==888]
+pas=data[data['PassengerId']==889]
 print(pas['FamilySize'])
 
-# 887    1
+# 888    4
